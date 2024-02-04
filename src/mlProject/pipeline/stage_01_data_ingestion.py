acfa5d70fc3b29af4af2782fa21ee4413698e69b
Nathan Moderwell <nathan.moderwell@gmail.com>
2024-02-04 12:24:27 -0600

    build out the data ingestion component

--- src/mlProject/pipeline/stage_01_data_ingestion.py
+++ src/mlProject/pipeline/stage_01_data_ingestion.py
@@ -2,7 +2,13 @@ from mlProject.config.configuration import ConfigurationManager
 from mlProject.components.data_ingestion import DataIngestion
 from mlProject import logger
 
+
+
+
+
+
 STAGE_NAME = "Data Ingestion stage"
+
 
 class DataIngestionTrainingPipeline:
    def __init__(self):
@@ -15,6 +21,9 @@ class DataIngestionTrainingPipeline:
        data_ingestion = DataIngestion(config=data_ingestion_config)
        data_ingestion.download_file()
        data_ingestion.extract_zip_file()
+
+
+
 
   
 if __name__ == '__main__':
